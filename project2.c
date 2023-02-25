ffdf1402d6dedc141617a681445ecef73a12a847
ShawnScience <ScientificaShawn@gmail.com>
2023-02-24 20:05:44 -0500

    real second commit

--- project2.c
+++ project2.c
@@ -1,22 +1,49 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include <stdio.h>  //for printf()
+#include <string.h> //For Strings
+#include <stdlib.h> //For exit()
+#include <stdbool.h> //for boolean flags
 
 int main(int argc, char *argv[])
 {
-  if(argc > 1)
+  bool ikeep_go = true; //to escape interactive mode
+  bool fkeep_go = true; // to escape file mode
+  char *path = "/bin"; //initial shell path
+  
+  if(argc > 2)
     {
       fprintf(stderr, "Error, too many arguments to wish.\n");
       exit(-3);
     }
-  if(argc == 0)
+  if(argc == 1) //Interactive Mode
     {
-      printf("We got 0 args");
-      exit(-1);
+      while(ikeep_go)
+	{
+	  printf("wish> ");
+	  getline(&i_line, &i_size, stdin);
+	  
+	}
+      exit(0); //Called exit function.
     }
-  if(argc == 1)
+  if(argc == 2) //File Mode
     {
       printf("Got 1 arg");
       exit(2);
+      /* while(fkeep_go)
+	{
+	 //close(0&1) then try to open file then pipe and fork
+	}
+	exit(0); //Called exit function.
+       */
     }
+}
+
+void read_lines(FILE *fd_in, FILE *fd_out)
+{
+  char *line = NULL; //The line we read. 
+  size_t size = 0; //line size. getline uses ralloc
+}
+
+void exec_cmnds(char **cmnds)
+{
+  
 }
